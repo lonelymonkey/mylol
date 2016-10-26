91539f5d235501dbf66a71579179009e1ed43d64
wayne <wayne@rewards1.com>
2016-10-26 14:17:33 -0700

    added event register functions support for summonerBase

--- html/summonerBase.js
+++ html/summonerBase.js
@@ -4,7 +4,14 @@
   var config;
   var dataModel = {
     summonerSummary : []
-  }
+  };
+  //export dataModel
+  summonerBase.dataModel = dataModel;
+
+  var eventsCallback = {
+    'onLoad' : []
+  };
+
   var dataFlag = false;
 
   var search = 'epiccookierawr';
@@ -13,16 +20,31 @@
     config = $.extend({},cfg);
   }
 
+  function executeRegisteredCallbacks(event,data) {
+    eventsCallback[event].forEach(function(callback, index){
+      callback(data);
+    });
+  }
+  summonerBase.registerEvent = function(event, callback) {
+    if (typeof(callback) != 'function') return;
+    eventsCallback[event].push(callback);
+  }
+
+
   summonerBase.load = function(cfg){
     var dataAPISummoner = lolSummonerData(cfg);
     saveConfig(cfg);
     dataAPISummoner.summonerSummary(search,'SEASON2016',function(res){
       dataModel.summonerSummary = res;
-      summonerBase.buildView(res);
+      //summonerBase.buildView(res);
+      executeRegisteredCallbacks('onLoad',res);
       //console.log(JSON.stringify(summonerBase.dataModel));
       //console.log(JSON.stringify(dataModel));
     });
   };
+
+
+
 
 
 
--- html/summonerSummary.js
+++ html/summonerSummary.js
@@ -1,11 +1,24 @@
 (function(factory){
   window.summonerBase = factory(window.summonerBase);
 }(function(summonerBase){
+  var dataModel = summonerBase.dataModel;
 
+  function buildView(data) {
+    //check if dataModel changed?
+    //if unchanged return
+    console.log(dataModel);
+  }
+
+  summonerBase.registerEvent('onLoad',function(data){
+    console.log('build Summoner Summary view');
+    buildView(data);
+  });
+  //summonerBase.registerEvent('onLoad',buildView);
+  /*
   summonerBase.buildView = function(data){
     var data = data;
     console.log(data);
-  }
+  }*/
 
   return summonerBase;
 }))
