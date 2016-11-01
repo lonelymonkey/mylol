f04667cb07f2d088304d575ed1be13d6190e4ae0
billchou <bell10142006@hotmail.com>
2016-11-01 12:23:11 -0700

    added click to every rune in the list

--- html/summonerRunes.js
+++ html/summonerRunes.js
@@ -3,46 +3,73 @@
 }(function(summonerBase){
   var dataModel = summonerBase.dataModel;
   var config = {};
+  var allpages = [];
 
   function bindEvent() {
+    var display = 'teemo';
+    var runeId = '';
+    $('.rune-page').click(function(){
+      $('#runes-detail').empty();
+      runeId = $(this).html();
+      $.each(allpages,function(key,page){
+        if(page.name == runeId){
+          buildRunesDetail(page.slots);
+        }
+      });
+    });
+  }
+
+  function buildRunesDetail(data){
+    var runes = {};
+    var detail = '<ul>';
+    $.each(data,function(key,rune){
+      if(!(rune.runeName in runes)){
+        runes[rune.runeName] = 1;
+      }
+      else{
+        runes[rune.runeName] += 1;
+      }
+    });
+    console.log(runes);
+    $.each(runes,function(key,sum){
+      detail += '<li>'+key+sum+'</li>';
+    });
+    detail += '</ul>';
+    console.log(detail);
+    $('#runes-detail').append(detail);
   }
 
   function buildRunesView(){
-    console.log('buildRunesView');
+    //console.log('buildRunesView');
     var view = '';
     view += '' +
       '<div id="runes-list">'+
       '</div>'+
       '<div id="runes-detail">'+
       '</div>';
-      console.log(view);
+      //console.log(view);
 
     $('#summary-info').append(view);
   }
 
   function buildRunesList(){
     var id = dataModel.summonerSummary.summonerInfo[summonerBase.search]['id'];
-    console.log(dataModel);
     var data = dataModel.runes.summonerRunes[id]['pages'];
     var list = '<ul class="list-group">';
     var detail = '';
-    var sum = [];
 
   //  console.log(data);
     $.each(data,function(key, runePage){
-      list += '<li class="list-group-item">'+runePage.name+'</li>';
-      $.each(runePage.slots,function(key,rune){
-        detail += '<div>'+rune.runeName+'</div>';
-      });
+      list += '<li class="list-group-item"><button class="rune-page">'+runePage.name+'</button></li>';
+      allpages.push(runePage);
     });
     list += '</ul>';
-
+    console.log(allpages);
     $('#runes-list').append(list);
-    $('#runes-detail').append(detail);
   }
 
   summonerBase.registerEvent('runes',function(data){
-    console.log('build summoner runes view');
+    //console.log('build summoner runes view');
     buildRunesView();
     buildRunesList();
     bindEvent();
